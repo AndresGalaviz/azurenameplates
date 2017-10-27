6f1d2b2fa0589959b758075e74b4530f3346835c
Andres Galaviz <angalavi@microsoft.com>
2017-10-27 11:27:02 -0700

    Adding languages

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+
+var MsTranslator = require('mstranslator');
+// Second parameter to constructor (true) indicates that 
+// the token should be auto-generated. 
+ 
+var client = new MsTranslator({
+  api_key: "5e4afa9bf72c40508cb4fbee3b47f824"
+}, true);
+ 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('signup', { title: 'Azure Compute Give - Nameplates' });
 });
 
 router.post('/register', function(req, res, next) {
-    console.log(req.connection.user);
     // An object of options to indicate where to post to
-    var whoUrl = 'http://who/Data/Person/' + req.body.alias + '.xml';
-    console.log(whoUrl);
-    res.render('thanks');
+    var params = {
+      text: req.body.name,
+      from: 'en',
+      to: 'hi'
+    };
+    console.log(req.body)
+    // Don't worry about access token, it will be auto-generated if needed. 
+    client.translate(params, function(err, data) {
+      console.log(params)
+      console.log(data);
+      res.render('thanks', {name: data});
+    });
+
 });
 
 module.exports = router;
